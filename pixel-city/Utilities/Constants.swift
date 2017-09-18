7c2a7339ffb32fcc6b04f066b628d87f79a50d76
daver234 <dave.rothschild@gmail.com>
2017-09-17 23:23:33 -0700

    - update key

--- pixel-city/Utilities/Constants.swift
+++ pixel-city/Utilities/Constants.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let apiKey = "f43655ad7cf1704d9d79fb05a3240605"
+let apiKey = ""
 
 func flickrUrl(forApiKey key: String, withAnnotation annoatation: DroppablePin, andNumberOfPhotos number: Int) -> String {
     return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(apiKey)&lat=\(annoatation.coordinate.latitude)&lon=\(annoatation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
